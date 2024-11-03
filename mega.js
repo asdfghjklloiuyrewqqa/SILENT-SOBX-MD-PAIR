58af3aa07aefbe4dc39a04600d9bac6ad3d7bcbd
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-03 11:34:39 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'EnterYourMegaEmail',
-    password: 'EnterYourMegaPassword',
+    email: 'princemds432@yahoo.com',
+    password: '002821002821#Silen',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
